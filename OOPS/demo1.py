5061348b5e949580a2ca08ac2e1eacf9c338cfbb
Ashlin Shaji <ashlinshaji43@gmail.com>
2025-07-02 13:09:56 +0530

    py ad

---
+++ OOPS/demo1.py
@@ -0,0 +1,16 @@
+class Person:
+    def setvalue(self,first_name,last_name,age,gender,phno):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.age=age
+        self.gender=gender
+        self.phno=phno
+    def printvalue(self):
+        print(self.first_name,self.last_name,self.age,self.gender,self.phno)
+person1=Person()
+person1.setvalue('vinay','k',26,'Male',97658439220)
+person1.printvalue()
+print("*"*100)
+person2=Person()
+person1.setvalue('Anu','M',23,'Female',97658439786)
+person1.printvalue()
