93e966e576bfa02ae2607e7f5eaaef33656d1559
Ashlin Shaji <ashlinshaji43@gmail.com>
2025-07-02 12:36:00 +0530

    py ad

---
+++ OOPS/OP1.py
@@ -0,0 +1,20 @@
+#Object Oriented Programming
+
+#class : blueprint or basic model
+
+#object : according to the class what we design or develop.It is called object
+
+#referennce : operation done in an object
+
+
+#eg
+
+# class --- TV
+
+# object ---  samsung,sony,lg,oneplus,nokia
+
+# references --- volume up,volume down, channel changes,etc
+
+
+
+
